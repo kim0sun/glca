373f17daa79571a1a924f556eec8167564c14dd7
kim0sun <kim0sun@korea.ac.kr>
2020-03-15 17:38:25 +0900

    likelihood0 added / code arranged

--- R/glca_gnr.R
+++ R/glca_gnr.R
@@ -2,64 +2,78 @@ glca_gnr <- function(
    model, param, datalist
 )
 {
-   if (model$W > 1) {
-      if (is.null(param$beta)) {
-         param$gamma = lapply(1:model$G, function(g)
-            lapply(1:model$W, function(w)
-               matrix(param$gamma[w, ], model$Ng[g], model$C, byrow = TRUE)))
+   Ng <- model$Ng; G <- model$G
+   C <- model$C; W <- model$W
+   M <- model$M; R <- model$R
+   P <- model$P; Q <- model$Q
+
+   delta <- param$delta
+   gamma <- param$gamma
+   beta  <- param$beta
+   rho   <- param$rho
+
+   if (W > 1) {
+      if (is.null(beta)) {
+         gamma = lapply(1:G, function(g)
+            lapply(1:W, function(w)
+               matrix(gamma[w, ], Ng[g], C, byrow = TRUE)))
       }
       # MLCA with constraint
-      class2 = sample(1:model$W, model$G, prob = param$delta,
+      class2 = sample(1:W, G, prob = delta,
                        replace = TRUE)
-      class = lapply(1:model$G, function(g) sapply(1:model$Ng[g], function(i)
-            sample(1:model$C, 1, prob = param$gamma[[g]][[class2[g]]][i, ])))
-      y = lapply(1:model$G, function(g) sapply(1:model$M, function(m)
+      class = lapply(1:G, function(g) sapply(1:Ng[g], function(i)
+            sample(1:C, 1, prob = gamma[[g]][[class2[g]]][i, ])))
+      y = lapply(1:G, function(g) sapply(1:M, function(m)
          sapply(class[[g]], function(c)
-            sample(1:model$R[m], 1, prob = param$rho[[m]][c, ]))))
-      for (g in 1:model$G)
+            sample(1:R[m], 1, prob = rho[[m]][c, ]))))
+      for (g in 1:G)
          y[[g]][datalist$y[[g]] == 0] = 0
    } else {
       # MGLCA with constraint
-      if (model$P == 1)
-         class = lapply(1:model$G, function(g) sapply(1:model$Ng[g], function(i)
-            sample(1:model$C, 1, prob = param$gamma[g, ])))
+      if (P == 1)
+         class = lapply(1:G, function(g) sapply(1:Ng[g], function(i)
+            sample(1:C, 1, prob = gamma[g, ])))
       else {
-         class = lapply(1:model$G, function(g) sapply(1:model$Ng[g], function(i)
-            sample(1:model$C, 1, prob = param$gamma[[g]][i, ])))
+         class = lapply(1:G, function(g) sapply(1:Ng[g], function(i)
+            sample(1:C, 1, prob = gamma[[g]][i, ])))
       }
-      y = lapply(1:model$G, function(g) sapply(1:model$M, function(m)
+      y = lapply(1:G, function(g) sapply(1:M, function(m)
          sapply(class[[g]], function(c)
-            sample(1:model$R[m], 1, prob = param$rho[[g]][[m]][c, ]))))
+            sample(1:R[m], 1, prob = rho[[g]][[m]][c, ]))))
 
-      for (g in 1:model$G)
+      for (g in 1:G)
          y[[g]][datalist$y[[g]] == 0] = 0
    }
 
-   npatt <- prod(model$R)
+   npatt <- prod(R)
    if (npatt < 1e+6) {
-      pattern <- expand.grid(lapply(1:model$M, function(m) 1:model$R[m]))
-      obsvd <- sapply(1:model$G, function(g)
-         ObsCell(y[[g]], model$Ng[g], model$M, model$R, 1000, 1e-8))
+      pattern <- expand.grid(c(lapply(1:M, function(m)
+         1:R[m]), list(1:G)))
+      obsvd <- unlist(lapply(1:G, function(g)
+         ObsCell(cbind(y[[g]], g), Ng[g], M, R, 1000, 1e-8)))
+      loglikg <- numeric(G)
+      for (g in 1:G)
+         loglikg[[g]] <- ObsLik(as.matrix(y[[g]]),
+                                Ng[g], M, R, 1000, 1e-8)
+
+      loglik0 <- sum(loglikg)
    } else {
-      Y = do.call(rbind, y)
+      Y = do.call(rbind, lapply(1:G, function(g) cbind(y[[g]], g)))
       Y0 <- Y[rowSums(Y == 0) == 0,]
-      Y.sorted <- Y0[do.call(order, data.frame(Y0)),]
+      Y.sorted <- Y0[do.call(order, data.frame(Y0)[(M + 1):1]),]
       pattern <- unique(Y.sorted)
 
-      y.sorted <- lapply(1:model$G, function(g)
-         y[[g]][do.call(order, data.frame(y[[g]]))])
-      obsvd <- lapply(1:model$G, function(g)
-         ObsCell2(as.matrix(y.sorted[[g]]), as.matrix(pattern),
-                  model$Ng[g], nrow(pattern)))
+      obsvd <- unlist(ObsCell2(as.matrix(Y.sorted), as.matrix(pattern),
+                               nrow(Y.sorted), nrow(pattern)))
+      loglikg <- numeric(G)
+      for (g in 1:G) {
+         tmp = obsvd[pattern[, M + 1] == g]
+         loglikg[[g]] <- sum(tmp * log(tmp / sum(tmp)))
+      }
+      loglik0 <- sum(loglikg)
    }
-
-   if (!is.null(model$W)) {
-      obsvd <- rowSums(obsvd)
-   }
-
-   llik0 = sum((obsvd * log(obsvd / sum(obsvd)))[obsvd != 0])
 
    return(list(y = y, x = datalist$x, z = datalist$z,
                pattern = as.matrix(pattern), observed = obsvd,
-               loglike = llik0))
+               loglike = loglik0))
 }
