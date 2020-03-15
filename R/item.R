cdee70f7122965e4d144d0dfe596c24245d3929d
kim0sun <kim0sun@korea.ac.kr>
2020-03-15 17:39:58 +0900

    starts/ends.with added.

--- R/item.R
+++ R/item.R
@@ -3,11 +3,11 @@
 #' Specifying manifest items in formula of \code{glca} function.
 #'
 #' @param \dots vectors of manifest items. These can be given as named arguments which is colnames of \code{data.frame}.
+#' @param starts.with a string
+#' @param ends.with a string
 #'
 #' @return
-#' a \code{matrix} of specified variables, which contains following attributes about items.
-#'
-#' Names and levels of manifest items.
+#' a \code{matrix} of specified variables, which contains names and levels of manifest items.
 #'
 #' @seealso \code{\link{glca}}
 #'
@@ -16,18 +16,45 @@
 #'
 #' @export
 
-item <- function(...)
+item <- function(..., starts.with = NULL, ends.with = NULL)
 {
-   obj <- substitute(list(...))
-   args <- list(...)
-   argf <- lapply(args, as.factor)
+   if (!is.character(starts.with) && !is.character(ends.with)) {
+      obj <- substitute(list(...))
+      args <- list(...)
+      argf <- lapply(args, as.factor)
 
-   y <- do.call("cbind", lapply(argf, as.numeric))
-   y[is.na(y)] <- 0
+      y <- do.call("cbind", lapply(argf, as.numeric))
+      if (is.null(y))
+         stop("No manifest items were entered.")
+      y[is.na(y)] <- 0
 
-   attr(y, "y.names") <- sapply(obj, deparse)[-1]
-   attr(y, "y.level") <- lapply(argf, levels)
-   class(y) <- "items"
+      attr(y, "y.names") <- sapply(obj, deparse)[-1]
+      attr(y, "y.level") <- lapply(argf, levels)
+      class(y) <- "items"
+   } else {
+      sname <- ename <- name <- ls(parent.frame())
+
+      if (is.character(starts.with)) {
+         sname <- name[startsWith(name, starts.with)]
+      }
+      if (is.character(ends.with)) {
+         ename <- name[endsWith(name, ends.with)]
+      }
+
+      name <- intersect(sname, ename)
+      if (length(name) == 0)
+         stop("No variables match with starts.with or ends.with")
+
+      args <- lapply(name, get, envir = parent.frame())
+      argf <- lapply(args, as.factor)
+
+      y <- do.call("cbind", lapply(argf, as.numeric))
+      y[is.na(y)] <- 0
+
+      attr(y, "y.names") <- name
+      attr(y, "y.level") <- lapply(argf, levels)
+      class(y) <- "items"
+   }
 
    return(y)
 }
