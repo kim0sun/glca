349e81775b78279f7465cadedd929a84e8185771
kim0sun <kim0sun@korea.ac.kr>
2020-03-18 18:40:22 +0900

    imports

--- R/package.R
+++ R/package.R
@@ -3,7 +3,7 @@
 #' Fitting latent class models for data containing group variable. \cr Standard LCA (LCA), multiple-group LCA (MGLCA) and multilevel LCA (MLCA) can be fitted.
 #'
 #' @docType package
-#' @import Rcpp MASS stats
+#' @import Rcpp MASS stats graphics grDevices
 #' @importFrom Rcpp evalCpp
 #' @useDynLib glca, .registration = TRUE
 #' @name glca-package
