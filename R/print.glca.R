1d4e2ccd941ea74a106860612c660ab839647f30
kim0sun <kim0sun@korea.ac.kr>
2021-03-03 02:43:56 +0900

    print edited

--- R/print.glca.R
+++ R/print.glca.R
@@ -12,39 +12,25 @@ print.glca = function(x, ...)
    cat("\nCall:\n",  paste(deparse(x$call), sep = "\n", collapse = "\n"),
        "\n\n", sep = "")
 
-   cat("Manifest items :\n", var.names$y.names, "\n")
+   cat("Manifest items :", var.names$y.names, "\n")
    if (!is.null(x$call$group)) cat("Grouping variable :", x$call$group, "\n")
-   if (model$W > 0L) {
-      if (model$Q > 0L) cat("Covariates (Level 2) : \n", var.names$Z.names, "\n")
-      if (model$P > 1L) cat("Covariates (Level 1) : \n", var.names$X.names, "\n")
-   } else if (model$P > 1L) cat("Covariates : \n", var.names$X.names, "\n")
+   if (model$P > 1L) cat("Covariates (Level 1) :", var.names$X.names, "\n")
+   if (model$Q > 0L) cat("Covariates (Level 2) :", var.names$Z.names, "\n")
 
-   cat("\nModel :", model$type, "\n")
+   cat("\nModel :", model$type, "\n\n")
    if (model$W > 1L){
       cat("Number of latent classes :", model$C, "\n")
       cat("Number of latent clusters :", model$W, "\n")
-      cat("\nMean prevalence for latent clusters:\n")
-      print(round(colMeans(posterior$cluster), 5L))
-      cat("\nMean prevalence for latent classes:\n")
-      print(round(posterior$wclass, 5L))
-      cat("\n")
    } else {
       cat("Number of latent classes :", model$C, "\n")
-      cat("\nMean prevalence for latent classes:\n")
-      prev = as.matrix(do.call(rbind, lapply(posterior, colMeans)))
-      dimnames(prev) = list(var.names$g.names,
-                            paste0("Class ", 1L:model$C))
-      print(round(prev, 5L))
-      cat("\n")
    }
-
+   if (model$G > 1)
+      cat("Number of groups :", model$G, "\n")
    cat("Number of observations :", model$N, "\n")
-   cat("  Number of parameters :", model$npar, "\n")
-   if (model$G > 1L)
-      cat("      Number of groups :", model$G, "\n")
+   cat("Number of parameters :", model$npar, "\n")
 
    cat("\nlog-likelihood :", gof$loglik,
        "\n     G-squared :", gof$Gsq,
        "\n           AIC :", gof$aic,
-       "\n           BIC :", gof$bic, "\n\n")
+       "\n           BIC :", gof$bic, "\n")
 }
--- R/summary.glca.R
+++ R/summary.glca.R
@@ -29,23 +29,24 @@ summary.glca <- function(
    cat("\nCall:\n",  paste(deparse(object$call), sep = "\n", collapse = "\n"),
        "\n\n", sep = "")
 
-   cat("Manifest items : ", var.names$y.names, "\n")
+   cat("Manifest items :", var.names$y.names, "\n")
    if (!is.null(object$call$group)) cat("Grouping variable :", object$call$group, "\n")
-   if (model$P > 1L) cat("Covariates (Level 1) : ", var.names$X.names, "\n")
-   if (model$Q > 0L) cat("           (Level 2) : ", var.names$Z.names, "\n")
+   if (model$P > 1L) cat("Covariates (Level 1) :", var.names$X.names, "\n")
+   if (model$Q > 0L) cat("Covariates (Level 2) :", var.names$Z.names, "\n")
 
    cat("\nCategories for manifest items :\n")
    print(var.names$resp.name)
 
    cat("\nModel :", model$type, "\n\n")
    if (model$W > 1L){
-      cat("Number of latent classes  :", model$C, "\n")
+      cat("Number of latent classes :", model$C, "\n")
       cat("Number of latent clusters :", model$W, "\n")
    } else {
       cat("Number of latent classes :", model$C, "\n")
    }
    if (model$G > 1)
       cat("Number of groups :", model$G, "\n")
+   cat("Number of observations :", model$N, "\n")
    cat("Number of parameters :", model$npar, "\n")
 
    cat("\nlog-likelihood :", gof$loglik,
